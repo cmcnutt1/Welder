58024b8ebef1860164196a49fd57609eb5fee7b4
Chris McNutt <cmcnutt1@uncc.edu>
2017-01-27 01:50:28 -0500

    Fixed bug in last commit. Firebase integration for signing up is functional now.

--- app/src/main/java/smith/welder/LandingScreen.java
+++ app/src/main/java/smith/welder/LandingScreen.java
@@ -8,13 +8,21 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.spotify.sdk.android.authentication.AuthenticationClient;
 import com.spotify.sdk.android.authentication.AuthenticationResponse;
 
+import java.util.Map;
 import java.util.concurrent.Executor;
 
 import kaaes.spotify.webapi.android.SpotifyApi;
 import kaaes.spotify.webapi.android.SpotifyService;
+import kaaes.spotify.webapi.android.models.Pager;
+import kaaes.spotify.webapi.android.models.PlaylistSimple;
 import kaaes.spotify.webapi.android.models.UserPrivate;
 import retrofit.Callback;
 import retrofit.RetrofitError;
@@ -26,6 +34,9 @@ public class LandingScreen extends AppCompatActivity {
 
     private static String authToken;
     private static SpotifyApi webAPI;
+
+    private String discoverId;
+    private boolean newUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -60,14 +71,41 @@ public class LandingScreen extends AppCompatActivity {
                     //Toast.makeText(this, getAuthToken(), Toast.LENGTH_LONG).show();
                     Log.d("Authentication","got access token");
 
-                    String userID = "";
 
+                    //Begin setup for Android Web API wrapper
                     webAPI = new SpotifyApi();
 
                     webAPI.setAccessToken(toke);
 
-                    new UserLookup(userID,this).execute(webAPI);
+                    final SpotifyService web = webAPI.getService();
 
+
+                    //Everything nested in 'success' callback below. Otherwise,
+                    //HTTP GET curl request (Retrofit) will throw timing of
+                    //execution off, resulting in null username.
+
+                    web.getMe(new Callback<UserPrivate>() {
+                        @Override
+                        public void success(UserPrivate userPrivate, Response response) {
+                            String userID = userPrivate.id;
+                            Log.d("UserID",userID);
+
+                            boolean isNewUser = isUserRegistered(userID);
+
+                            if(isNewUser) {
+                                Log.d("Firebase", "User " + userID + " is a returning user. Proceeding..");
+                            }
+
+
+                            startActivity(new Intent(LandingScreen.this, MainMenu.class));
+
+                        }
+
+                        @Override
+                        public void failure(RetrofitError error) {
+                            Log.d("Error Getting User ID", error.toString());
+                        }
+                    });
 
 
                     //Log.d("Username", "Is: " + id);
@@ -87,4 +125,105 @@ public class LandingScreen extends AppCompatActivity {
         authToken = token;
     }
 
+    public boolean isUserRegistered(String username){
+
+        final String userID = username;
+
+        FirebaseDatabase db = FirebaseDatabase.getInstance();
+
+        String DBLocation = "/Welder/Users/";
+
+        final DatabaseReference dbRef = db.getReference(DBLocation);
+
+        dbRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                boolean userExists = false;
+
+                //Search for user in database
+                for (DataSnapshot child : dataSnapshot.getChildren()) {
+
+                    Log.d("UserInfo:",child.toString());
+                    Map<String, Object> model = (Map<String, Object>) child.getValue();
+
+                    if(model.get("username").equals(userID)) {
+                        userExists = true;
+                        break;
+                    }
+                }
+
+                if(userExists){
+                    //Do nothing.
+                    //User is in database
+                    Log.d("Firebase","User " + userID + " already in database");
+                    Log.d("Firebase stuff",dataSnapshot.toString());
+                }
+                else{
+                    writeNewUserToDatabase(dbRef,userID);
+                }
+
+                newUser = !userExists;
+
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.d("Firebase","Data write cancelled: " + databaseError.toString());
+            }
+
+        });
+
+        return newUser;
+    }
+
+    public void writeNewUserToDatabase(DatabaseReference dbRef, String username){
+
+        final String userID = username;
+        //Push new user object to database
+        dbRef.push().child("username").setValue(userID, new DatabaseReference.CompletionListener() {
+            @Override
+            public void onComplete(DatabaseError databaseError, DatabaseReference databaseReference) {
+                Log.d ("Firebase",("User " + userID + " has been created in database"));
+                Log.d("Firebase DBREF",databaseReference.toString());
+                writeUserDiscoverID(databaseReference);
+
+            }
+        });
+    }
+
+    public void writeUserDiscoverID(DatabaseReference dbr){
+
+        SpotifyService web = webAPI.getService();
+
+        final DatabaseReference dbRef = dbr;
+
+        web.getMyPlaylists(new Callback<Pager<PlaylistSimple>>() {
+            @Override
+            public void success(Pager<PlaylistSimple> playlistSimplePager, Response response) {
+                String discUri = "";
+                for(int i = 0; i < playlistSimplePager.items.size(); i++){
+                    if(playlistSimplePager.items.get(i).name.equals("Discover Weekly")){
+                        discUri = playlistSimplePager.items.get(i).uri;
+                        break;
+                    }
+                }
+                discoverId = discUri.substring(30);
+
+                dbRef.getParent().child("discoverId").setValue(discoverId, new DatabaseReference.CompletionListener() {
+                    @Override
+                    public void onComplete(DatabaseError databaseError, DatabaseReference databaseReference) {
+                        Log.d("Firebase","Hopefully this works. discover id: " + discoverId);
+                    }
+                });
+            }
+
+            @Override
+            public void failure(RetrofitError error) {
+
+            }
+        });
+
+    }
 }
